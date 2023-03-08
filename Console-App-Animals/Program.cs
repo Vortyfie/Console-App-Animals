6d5332a2e2d36e16a693efa5bb68eb99389bd8c7
Vortyfie <bastien.dasilva@icloud.com>
2023-03-08 11:05:35 +0100

    TP 1

--- Console-App-Animals/Program.cs
+++ Console-App-Animals/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Console_App_Animals;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal chien = new Animal();
+        Animal chat = new Animal("TOTO");
+
+        chien.Sleep();
+        chat.Sleep();
+
+        Console.WriteLine();
+        Console.ReadLine();
+
+    }
+}
